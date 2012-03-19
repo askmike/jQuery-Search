f5cd69df87f7cda133254ee485afce4efefd3558
Mike van Rossum <mike@askmike.org>
2012-03-19 18:22:39 +0100

    major update
    You can now use the plugin to search using the plugin, instead of just
    using the input

--- filter.js
+++ filter.js
@@ -38,225 +38,245 @@ first create an object:
 */
 ;(function( $ ){
 
-  $.fn.search = function( parameter ) {  
-	
-	// prepare paremeter	
-	var 
-		wordsParam = [],
-		options,
-		inited = false;
-	
-	if( typeof(parameter) === 'string' ) {
-		wordsParam = parameter.split(' ');
-	} else if( typeof(parameter) === 'array' ) {
-		wordsParam = parameter;
-	} else if ( typeof(parameter) === 'object' ) {
-		options = parameter;
-	}
-	
-    // define default settings
-    var settings = $.extend({
-		'container': '#templates',
-		'single': '.single',
-		'singleStructure': [
-			{
-				'elem': 'h3',
-				'score': 3
-			},
-			{
-				'elem': 'textarea',
-				'score': 1
-			}
-		]
-    }, options);
-	
-	// define all private methods and properties
-	var 
-		output,
-		search = this,
-		singleElems = [],
-		templates = [],
-		included = [],
-		scoreBoard = [],
-		container = $( settings.container ),
-		singles = container.find( settings.single ),
-		/*
-					These are all internal (private) methods
-		*/
-		/*
-			this function searches the DOM for all templates 
-			and adds them to the templates array
-		*/
-		init = function() {
-			
-			var structure = settings.singleStructure;
+	$.fn.search = function( parameter ) {  
+		
+		// prepare paremeter	
+		var wordsParam = [],
+			options,
+			data = this.data('search'),
+			search = this,
+			output;
 
-			//put all the score multipliers in scoreboard and cache all elems
-			for(var len = structure.length, i = 0; i < len; i++) {
-				scoreBoard.push(structure[i].score);
-				singleElems.push(structure[i].elem);
-			}
+		if( typeof parameter === 'string' ) {
+			wordsParam = parameter.split(' ');
+		} else if( typeof parameter === 'array' ) {
+			wordsParam = parameter;
+		} else if ( typeof parameter === 'object' ) {
+			options = parameter;
+		}
 
-			//take singles off dom, give them a start position, store the (to be indexed) text and re-attach to the dom
-			singles.detach().each(function(i){
-
-				$( this ).data( 'position', i );
-
-				var temp = [];
-
-				for( var len = singleElems.length, j = 0; j < len; j++ ) {
-					temp.push( singles.eq( i ).find(singleElems[j]).text() );
-				}
-
-				templates.push({
-					text: temp,
-					score: 0
-				});
-			}).appendTo( container );
-
-			//if an search input is provided, let's watch it.
-			watchInput();
-			
+		// define default settings
+		var 
+			settings = $.extend({
+			'container': '#templates',
+			'single': '.single',
+			'singleStructure': 
+				[
+					{
+						'elem': 'h3',
+						'score': 3
+					},
+					{
+						'elem': 'textarea',
+						'score': 1
+					}
+				]
+			}, options),
+			data = search.data('search'),
 			inited = true;
-			output = templates.length;
-		},
-		/*
-			This function takes an array of words as input and 
-		*/
-		query = function( words ) {
+
+		if( !data ) {
 			
-			//reset the score and the results array
-			clean();
+			var container = $( settings.container );
+			
+			inited = false;
 
-			// every word
-			for( var lenI = words.length, i = 0; i < lenI; i++ ) {
-				var	word = words[i];
+			data = {
+				search: search,
+				singleElems: [],
+				templates: [],
+				included: [],
+				scoreBoard: [],
+				container: container,
+				singles: container.find( settings.single ),
+				settings: settings
+			};
+			
+			var
+				/*
+				this function searches the DOM for all templates 
+				and adds them to the templates array
+				*/
+				init = function() {
 
-				// in every template
-				for( var lenJ = templates.length, j = 0; j < lenJ; j++ ) {
+					var structure = data.settings.singleStructure;
 
-					var template = templates[j],
-						filter = new RegExp( '\\b' + word + '\\b', 'gi' );
+					//put all the score multipliers in scoreboard and cache all elems
+					for( var len = structure.length, i = 0; i < len; i++ ) {
+						data.scoreBoard.push( structure[i].score );
+						data.singleElems.push( structure[i].elem );
+					}
+			
+					// take singles off dom, give them a start position, store the (to be indexed) text and re-attach to the dom
+					data.singles.detach().each(function(i){
+					
+						$( this ).data( 'position', i );
+					
+						var temp = [];
+					
+						for( var len = data.singleElems.length, j = 0; j < len; j++ ) {
+							temp.push( data.singles.eq(i).find(data.singleElems[j]).text() );
+						}
+					
+						data.templates.push({
+							text: temp,
+							score: 0
+						});
+						
+					}).appendTo( data.container );
+					
+					// if an search input is provided, let's watch it.
+					watchInput();
+					output = data.templates.length;
 
+					search.data('search', data);
 
-					// in every element of every template
-					for( var lenK = singleElems.length, k = 0; k < lenK; k++ ) {
-
-						if( filter.test(template.text[k]) ) {
-							//the word is in the elem
-							template.score += scoreBoard[k];
-
-							//if it's a new word
-							if( $.inArray( j, included ) < 0 ){
-								//it's not in the array yet
-								included.push( j );
+				};
+			}
+				
+				
+			
+			/*
+			This function takes an array of words as input and 
+			*/
+			var	query = function( words ) {
+			
+					//reset the score and the results array
+					clean();
+			
+					// every word
+					for( var lenI = words.length, i = 0; i < lenI; i++ ) {
+						var	word = words[i];
+			
+						// in every template
+						for( var lenJ = data.templates.length, j = 0; j < lenJ; j++ ) {
+			
+							var template = data.templates[j],
+							filter = new RegExp( '\\b' + word + '\\b', 'gi' );
+			
+			
+							// in every element of every template
+							for( var lenK = data.singleElems.length, k = 0; k < lenK; k++ ) {
+			
+								if( filter.test(template.text[k]) ) {
+									//the word is in the elem
+									template.score += data.scoreBoard[k];
+			
+									//if it's a new word
+									if( $.inArray( j, data.included ) < 0 ){
+										//it's not in the array yet
+										data.included.push( j );
+									}
+								}
 							}
 						}
 					}
-				}
-			}
-
-			//remove low hits
-			var treshold = words.length; //Math.floor((words.length + 1) / 2);
-
-			build( treshold );
-		},
-		/*
-			this funcion resets all scores to 0
-		*/
-		clean = function() {
-			for( var i = templates.length; i-- ; ) {
-				templates[i].score = 0;
-			}
-
-		},
-		/*
-			this functions hides all singles that are either not in included or have a to low score
-			it additionally sorts all singles based on the score
-		*/
-		build = function( treshold ) {
-
-			function scoreSort( a, b ) {
-			    return $( a ).data( 'score' ) < $( b ).data( 'score' ) ? 1 : -1;
-			}
-
-			function positionSort( a, b ) {
-			    return $( a ).data( 'position' ) > $( b ).data( 'position' ) ? 1 : -1;
-			}
-			output = 0;
 			
-			// take them off dom, manip and put them back
-			// we need to sort because templates needs to be mapped to singles
-			singles.detach().sort( positionSort ).each(function(i) {
-				var $this = $( this );
-
-				$this.hide().data( 'score', templates[i].score );
-
-				// console.log('pos: ' + $this.data('position') + '\tscore: ' + $this.data('score') + '\t' + $this.find('h3').text())
-
-				//if this template is not included or if this index number does not meet the treshold
-				if( $.inArray(i, included) != -1 && treshold <= templates[i].score ) {
-					$this.show();
-					output++;
-				}
-			});
-
-			singles.sort( scoreSort );
-			container.append( singles );
-
-			//we need to requery because we just changed them
-			singles = container.find( settings.single );
-
-		},
-		/*
-			this function watches an input field for 
-			spacebars and filters upon
-		*/
-		watchInput = function() {
-			search.keyup(function( e ) {
-
-				//if input is empty display all results
-				if( search.val() == '' ) {
-					query([' ']);
-					return;
-				}
-
-				if( e.which == 32 ) { // = spacebar
-					var
-						words = [],
-						input = search.val().split(' ');
-
-					for( var i = 0, len = input.length; i < len; i++ ) {
-						if( input[i] != '' ) {
-							words.push( input[i] );
-						}
+					//remove low hits
+					var treshold = words.length; //Math.floor((words.length + 1) / 2);
+			
+					build( treshold );
+				},
+				/*
+				this funcion resets all scores to 0
+				*/
+				clean = function() {
+					for( var i = data.templates.length; i-- ; ) {
+						data.templates[i].score = 0;
 					}
-
+			
+				},
+				/*
+				this functions hides all singles that are either not in included or have a to low score
+				it additionally sorts all singles based on the score
+				*/
+				build = function( treshold ) {
+			
+					function scoreSort( a, b ) {
+						return $( a ).data( 'score' ) < $( b ).data( 'score' ) ? 1 : -1;
+					}
+			
+					function positionSort( a, b ) {
+						return $( a ).data( 'position' ) > $( b ).data( 'position' ) ? 1 : -1;
+					}
+					output = 0;
+			
+					// take them off dom, manip and put them back
+					// we need to sort because templates needs to be mapped to singles
+					data.singles.detach().sort( positionSort ).each(function(i) {
+						var $this = $( this );
+			
+						$this.hide().data( 'score', data.templates[i].score );
+			
+						// console.log('pos: ' + $this.data('position') + '\tscore: ' + $this.data('score') + '\t' + $this.find('h3').text())
+			
+						//if this template is not included or if this index number does not meet the treshold
+						if( $.inArray(i, data.included) != -1 && treshold <= data.templates[i].score ) {
+							$this.show();
+							output++;
+						}
+					});
+			
+					data.singles.sort( scoreSort );
+					data.container.append( data.singles );
+			
+					//we need to requery because we just changed them
+					data.singles = data.container.find( data.settings.single );
+			
+				},
+				/*
+				this function watches an input field for 
+				spacebars and filters upon
+				*/
+				watchInput = function() {
+					
+					var search = data.search;
+					
+					search.keyup(function( e ) {
+			
+						//if input is empty display all results
+						if( search.val() == '' ) {
+							query([' ']);
+							return;
+						}
+			
+						if( e.which == 32 ) { // = spacebar
+							var
+								words = [],
+								input = search.val().split(' ');
+			
+							for( var i = 0, len = input.length; i < len; i++ ) {
+								if( input[i] != '' ) {
+									words.push( input[i] );
+								}
+							}
+			
+							query( words );
+			
+						}
+					});
+				},
+				filter = function( words, input ) {
 					query( words );
-
+			
+					//add query to input
+					if( !input && words[0] != ' ' ) { //it's not a reset
+					var string = words.join(' ') + ' ';
+					data.search.val( string );
 				}
-			});
-		}, filter = function( words, input ) {
-			query( words );
+			};
 
-			//add query to input
-			if( !input && words[0] != ' ' ) { //it's not a reset
-				var string = words.join(' ') + ' ';
-				search.val( string );
+
+			// init if its the first time
+			if( !inited ) {
+				init();
+			} 
+			
+			// filter if we searched for words
+			if( wordsParam.length) {
+				query( wordsParam );
 			}
+			
+			return output;
 		};
-	
-	// init if its the first time
-	if( !inited ) {
-		init();
-	} 
-	
-	// filter if we searched for words
-	if( wordsParam.length) {
-		query( wordsParam );
-	}
-	
-	return output;
-
-  };
 })( jQuery );
