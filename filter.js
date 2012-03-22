aa1b63e947413a75414a1df29fee68173ff05ed1
Mike van Rossum <mike@askmike.org>
2012-03-22 12:22:28 +0100

    extended comments for readability

--- filter.js
+++ filter.js
@@ -12,7 +12,9 @@
 		// prepare paremeter	
 		var wordsParam = [],
 			options,
+		// try getting the data from last time
 			data = this.data('search'),
+		// set the input elem
 			search = this,
 			output;
 
@@ -45,31 +47,32 @@
 
 		if( !data ) {
 			
-			var container = $( settings.container );
-			
+			// data isn't there, this is the first run
 			inited = false;
-
-			data = {
-				search: search,
-				singleElems: [],
-				templates: [],
-				included: [],
-				scoreBoard: [],
-				container: container,
-				singles: container.find( settings.single ),
-				settings: settings
-			};
+			
+			var container = $( settings.container ),
+				data = {
+					search: search,
+					singleElems: [],
+					templates: [],
+					included: [],
+					scoreBoard: [],
+					container: container,
+					singles: container.find( settings.single ),
+					settings: settings
+				};
 			
 
 			/*
-			this function searches the DOM for all templates 
-			and adds them to the templates array
+				this function searches the DOM for all templates 
+				and adds them to the templates array
 			*/
 			var init = function() {
-
+				
+				// cache structure
 				var structure = data.settings.singleStructure;
 
-				//put all the score multipliers in scoreboard and cache all elems
+				// put all the score multipliers in scoreboard and cache all different elem structures
 				for( var len = structure.length, i = 0; i < len; i++ ) {
 					data.scoreBoard.push( structure[i].score );
 					data.singleElems.push( structure[i].elem );
@@ -78,6 +81,7 @@
 				// take singles off dom, give them a start position, store the (to be indexed) text and re-attach to the dom
 				data.singles.detach().each(function(i){
 				
+					// give each single an start position so we can reset after each search
 					$( this ).data( 'position', i );
 				
 					var temp = [];
@@ -92,19 +96,22 @@
 					});
 					
 				}).appendTo( data.container );
-				
-				watchInput();
-				output = data.templates.length;
 
+				// set up an key watcher
+				watchInput();
+				
+				// store the new data in the search elem for every future run
 				search.data('search', data);
+				
+				output = data.templates.length;
 
 			};
 		}
 				
 				
 			
-			/*
-		This function takes an array of words as input and 
+		/*
+			This function takes an array of words as input and matches those with all the indexed texts.
 		*/
 		var	query = function( words ) {
 		
@@ -139,13 +146,13 @@
 					}
 				}
 		
-				//remove low hits
+				// set a treshold to hide non relevant stuff
 				var treshold = words.length; //Math.floor((words.length + 1) / 2);
 		
 				build( treshold );
 			},
 			/*
-			this function resets all scores to 0
+				this function resets all scores to 0
 			*/
 			clean = function() {
 				for( var i = data.templates.length; i-- ; ) {
@@ -154,8 +161,8 @@
 		
 			},
 			/*
-			this functions hides all singles that are either not in included or have a to low score
-			it additionally sorts all singles based on the score
+				this functions hides all singles that are either not in included or have a to low score
+				it additionally sorts all singles based on the score
 			*/
 			build = function( treshold ) {
 		
@@ -172,12 +179,13 @@
 				// we need to sort because templates needs to be mapped to singles
 				data.singles.detach().sort( positionSort ).each(function(i) {
 					var $this = $( this );
-		
+					
+					// hide everything on default and set a score
 					$this.hide().data( 'score', data.templates[i].score );
 		
 					// console.log('pos: ' + $this.data('position') + '\tscore: ' + $this.data('score') + '\t' + $this.find('h3').text())
 		
-					//if this template is not included or if this index number does not meet the treshold
+					// if this template is included and if this index number does meet the treshold we display it
 					if( $.inArray(i, data.included) != -1 && treshold <= data.templates[i].score ) {
 						$this.show();
 						output++;
@@ -187,29 +195,29 @@
 				data.singles.sort( scoreSort );
 				data.container.append( data.singles );
 		
-				//we need to requery because we just changed them
+				// we need to requery because we just changed them
 				data.singles = data.container.find( data.settings.single );
 		
 			},
 			/*
-			this function watches an input field for 
-			spacebars and filters upon
+				this function watches an input field for 
+				spacebars and filters upon
 			*/
 			watchInput = function() {
 				
+				// cache the input field
 				var search = data.search;
 				
 				search.keyup(function( e ) {
 		
-					//if input is empty display all results
+					//if input is empty display all results in their original order
 					if( search.val() == '' ) {
 						query([' ']);
 						return;
 					}
 		
 					if( e.which == 32 ) { // = spacebar
-						var
-							words = [],
+						var words = [],
 							input = search.val().split(' ');
 		
 						for( var i = 0, len = input.length; i < len; i++ ) {
@@ -228,10 +236,10 @@
 		
 				//add query to input
 				if( !input && words[0] != ' ' ) { //it's not a reset
-				var string = words.join(' ') + ' ';
-				data.search.val( string );
-			}
-		};
+					var string = words.join(' ') + ' ';
+					data.search.val( string );
+				}
+			};
 
 
 		// init if its the first time
